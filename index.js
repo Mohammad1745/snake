1a86bde9a84b488be714ef541f71dbe9ee9d4a84
Mohammad Ali <mdali2016.227@gmail.com>
2021-05-18 12:25:24 +0600

    [Change] jquery to raw js

--- index.js
+++ index.js
@@ -1,61 +1,62 @@
-let row = 20;
-let column = 20;
-let snakeHeadPoint = [0,0];
-let snakeHead = snakeHeadPoint[0]*column+snakeHeadPoint[1];
-let snakeBody = [snakeHead];
-let snakeTail = snakeHead;
-let direction = 'R';
-let speed = 5;//dot per second
-let frog = 0;
-let gameMode = 0;
-let device = 'pc';
-let score = 0;
-let strikeAudio = new Audio("frog.mp3");
+let row = 20
+let column = 20
+let snakeHeadPoint = [0,0]
+let snakeHead = snakeHeadPoint[0]*column+snakeHeadPoint[1]
+let snakeBody = [snakeHead]
+let snakeTail = snakeHead
+let direction = 'R'
+let speed = 5//dot per second
+let frog = 0
+let gameMode = 0
+let device = 'pc'
+let score = 0
+let strikeAudio = new Audio("frog.mp3")
 
-$(document).ready (function () {
+document.addEventListener("DOMContentLoaded", () => {
     //rendering form
-    renderInputForm();
+    renderInputForm()
     //mouse click
-    $('#start_btn').on('click', function () {
+    document.querySelector('#start_btn').addEventListener('click', () => {
         if (gameMode===0 && checkInitialData()) {
-            gameMode=1;
-            $(this).html('Stop');
-            if (device==='mobile') renderController();
-            renderArena();
-            renderScore();
-            renderFrog();
-            activatePoint(snakeHead);
-            moveSnake();
+            gameMode=1
+            this.innerHTML = 'Stop'
+            if (device==='mobile') renderController()
+            renderArena()
+            renderScore()
+            renderFrog()
+            activatePoint(snakeHead)
+            moveSnake()
         } else if (gameMode===1 || gameMode===2 || gameMode===3) {
-            location.reload();
+            location.reload()
         }
-    });
-    $('#pause_btn').on('click', function () {
+    })
+    document.querySelector('#pause_btn').addEventListener('click', () => {
         if (gameMode===1) {
-            gameMode=2;
-            $(this).html('Resume');
+            gameMode=2
+            this.innerHTML = 'Resume'
         } else if (gameMode===2) {
-            gameMode=1;
-            $(this).html('Pause');
+            gameMode=1
+            this.innerHTML = 'Pause'
         }
-    });
+    })
     //catching keypress
-    $('body').keypress(function(event){
-        if (event.keyCode===119 && direction!=='D') direction = 'U';
-        else if (event.keyCode===97 && direction!=='R') direction = 'L';
-        else if (event.keyCode===115 && direction!=='U') direction = 'D';
-        else if (event.keyCode===100 && direction!=='L') direction = 'R';
-        else if (event.keyCode===112) $("#pause_btn").trigger('click');
-        // else alert(event.keyCode);
-    });
-    $('#control').on('click', '#up', function () {if (direction!=='D') direction = 'U'})
-    $('#control').on('click', '#left', function () {if (direction!=='R') direction = 'L'})
-    $('#control').on('click', '#down', function () {if (direction!=='U') direction = 'D'})
-    $('#control').on('click', '#right', function () {if (direction!=='L') direction = 'R'})
+    window.addEventListener('keydown', event => {
+        if (event.key==='w' && direction!=='D') direction = 'U'
+        else if (event.key==='a' && direction!=='R') direction = 'L'
+        else if (event.key==='s' && direction!=='U') direction = 'D'
+        else if (event.key==='d' && direction!=='L') direction = 'R'
+        else if (event.key==='p') document.querySelector("#pause_btn").click()
+        // else alert(event.keyCode)
+    })
+    let controller = document.querySelector('#control')
+    controller.querySelector('#up').addEventListener('click', () => {if (direction!=='D') direction = 'U'})
+    controller.querySelector('#left').addEventListener('click', () => {if (direction!=='R') direction = 'L'})
+    controller.querySelector('#down').addEventListener('click', () => {if (direction!=='U') direction = 'D'})
+    controller.querySelector('#right').addEventListener('click', () => {if (direction!=='L') direction = 'R'})
 
     function renderInputForm () {
-        $('#arena').html(`
-            <div class="form row">
+        document.querySelector('#arena').innerHTML =
+            `<div class="form row">
                 <div class="col-md-3 form-group">
                     <label for="row">Arena Height</label>
                     <input type="number" min="2" id="row" name="row" value="`+row+`" class="form-control">
@@ -75,26 +76,26 @@ $(document).ready (function () {
                         <option value="mobile">Mobile</option>
                     </select>
                 </div>
-            </div>`);
+            </div>`
     }
     function checkInitialData() {
-        row = $('#arena').find("input#row").val();
-        column = $('#arena').find("input#column").val();
-        speed = $('#arena').find("input#speed").val();
-        device = $('#arena').find("#device").val();
+        row = document.querySelector('#arena').querySelector("#row").value
+        column = document.querySelector('#arena').querySelector("#column").value
+        speed = document.querySelector('#arena').querySelector("#speed").value
+        device = document.querySelector('#arena').querySelector("#device").value
         if (!(row&&column&&speed))
-            alert("Empty Form Data");
-        return row&&column&&speed;
+            alert("Empty Form Data")
+        return row&&column&&speed
     }
     function renderArena() {
-        $('#arena').html('');
-        $('.arena').css('width', ''+(column*10+2));
+        document.querySelector('#arena').innerHTML = ''
+        document.querySelector('.arena').style.width = (column*10+2)+'px'
         for (let i=0; i<row*column; i++) {
-            $("#arena").append('<div id="arena_dot_'+i+'" class="arena-dot inactive-dot"></div>');
+            document.querySelector("#arena").insertAdjacentHTML('beforeend','<div id="arena_dot_'+i+'" class="arena-dot inactive-dot"></div>')
         }
     }
     function renderController() {
-        $('#control').html(`
+        document.querySelector('#control').innerHTML = `
             <div class="row">
                 <div class="col-12">
                     <button id="up" class="up">U</button>
@@ -110,70 +111,75 @@ $(document).ready (function () {
                 <div class="col-12">
                     <button id="down" class="down">D</button>
                 </div>
-            </div>
-        `);
+            </div>`
     }
     function renderScore() {
-        $('#score').html('Score: '+score);
-        $('#message').html("Score :)").css('color','green');
+        document.querySelector('#score').innerHTML = 'Score: '+score
+        document.querySelector('#message').innerHTML = "Score :)"
+        document.querySelector('#message').color = 'green'
         setTimeout(function () {
-            $("#message").html("");
-        },1000);
+            document.querySelector("#message").innerHTML = ""
+        },1000)
     }
     function moveSnake() {
-        if (gameMode===0) snakeHeadPoint = [0,0];
+        if (gameMode===0) snakeHeadPoint = [0,0]
         else if (gameMode===1) {
-            if (direction==='U') snakeHeadPoint[0]--;
-            else if (direction==='L') snakeHeadPoint[1]--;
-            else if (direction==='D') snakeHeadPoint[0]++;
-            else if (direction==='R') snakeHeadPoint[1]++;
-            snakeHead = snakeHeadPoint[0]*column+snakeHeadPoint[1];
-            snakeBody.unshift(snakeHead);
+            if (direction==='U') snakeHeadPoint[0]--
+            else if (direction==='L') snakeHeadPoint[1]--
+            else if (direction==='D') snakeHeadPoint[0]++
+            else if (direction==='R') snakeHeadPoint[1]++
+            snakeHead = snakeHeadPoint[0]*column+snakeHeadPoint[1]
+            snakeBody.unshift(snakeHead)
             if (snakeHead===frog){
-                score += parseInt(speed);
-                renderScore();
-                renderFrog();
+                score += parseInt(speed)
+                renderScore()
+                renderFrog()
             } else {
-                snakeTail = snakeBody.pop();
-                deactivatePoint(snakeTail);
+                snakeTail = snakeBody.pop()
+                deactivatePoint(snakeTail)
             }
         }
-        if (snakeHeadPoint[0]<0||snakeHeadPoint[0]>row-1||snakeHeadPoint[1]<0||snakeHeadPoint[1]>column-1) wallStrike();
-        else if (snakeBody.filter(x => x===snakeHead).length>1) bodyStrike();
+        if (snakeHeadPoint[0]<0||snakeHeadPoint[0]>row-1||snakeHeadPoint[1]<0||snakeHeadPoint[1]>column-1) wallStrike()
+        else if (snakeBody.filter(x => x===snakeHead).length>1) bodyStrike()
         else {
-            activatePoint(snakeHead);
+            activatePoint(snakeHead)
             setTimeout(function () {
-                moveSnake();
+                moveSnake()
             }, 1000/speed)
         }
     }
     function renderFrog () {
         do {
-            frog = Math.round(Math.random()*row*column);
-        } while (snakeBody.includes(frog));
-        $('#arena').find('#arena_dot_'+frog).removeClass('inactive-dot active-dot').addClass('frog');
+            frog = Math.round(Math.random()*row*column)
+        } while (snakeBody.includes(frog))
+        document.querySelector('#arena').querySelector('#arena_dot_'+frog).classList.remove('inactive-dot','active-dot')
+        document.querySelector('#arena').querySelector('#arena_dot_'+frog).classList.add('frog')
     }
     function activatePoint (point) {
-        $('#arena').find('#arena_dot_'+point).removeClass('inactive-dot frog').addClass('active-dot');
+        document.querySelector('#arena').querySelector('#arena_dot_'+point).classList.remove('inactive-dot','frog')
+        document.querySelector('#arena').querySelector('#arena_dot_'+point).classList.add('active-dot')
     }
     function deactivatePoint (point) {
-        $('#arena').find('#arena_dot_'+point).removeClass('active-dot frog').addClass('inactive-dot');
+        document.querySelector('#arena').querySelector('#arena_dot_'+point).classList.remove('active-dot','frog')
+        document.querySelector('#arena').querySelector('#arena_dot_'+point).classList.add('inactive-dot')
     }
     function wallStrike () {
-        gameMode = 3;
-        $('#start_btn').html('Restart');
-        if (direction==='U') $("#arena").css('border-top', 'red solid 1px');
-        else if (direction==='L') $("#arena").css('border-left', 'red solid 1px');
-        else if (direction==='D') $("#arena").css('border-bottom', 'red solid 1px');
-        else if (direction==='R') $("#arena").css('border-right', 'red solid 1px');
-        $('#message').html("Wall Strike :(").css('color','red');
-        strikeAudio.play();
+        gameMode = 3
+        document.querySelector('#start_btn').innerHTML = 'Restart'
+        if (direction==='U') document.querySelector("#arena").borderTop = 'red solid 1px'
+        else if (direction==='L') document.querySelector("#arena").borderLeft = 'red solid 1px'
+        else if (direction==='D') document.querySelector("#arena").borderBottom = 'red solid 1px'
+        else if (direction==='R') document.querySelector("#arena").borderRight = 'red solid 1px'
+        document.querySelector('#message').innerHTML = "Wall Strike :("
+        document.querySelector('#message').color = 'red'
+        strikeAudio.play()
     }
     function bodyStrike () {
-        gameMode = 3;
-        $('#start_btn').html('Restart');
-        $('#arena').find('#arena_dot_'+snakeHead).css('background-color','#c33');
-        $('#message').html("Body Strike :(").css('color','red');
-        strikeAudio.play();
+        gameMode = 3
+        document.querySelector('#start_btn').innerHTML = 'Restart'
+        document.querySelector('#arena').querySelector('#arena_dot_'+snakeHead).style.backgroundColor = '#c33'
+        document.querySelector('#message').innerHTML = "Body Strike :("
+        document.querySelector('#message').color = 'red'
+        strikeAudio.play()
     }
-});
+})
